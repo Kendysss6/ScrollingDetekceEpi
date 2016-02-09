d85cc65df5ad78efa362ebf13a081e230d6b250c
Kendysss6 <Kendysss6@users.noreply.github.com>
2016-02-09 15:18:34 +0100

    Prvni commit, bylo vytvořeno základní návrh GUI. Ted se musí přidat funkcionalita.

---
+++ app/src/main/java/com/example/havlicek/scrollingdetekceepi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.havlicek.scrollingdetekceepi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scroll_activity);
+    }
+}
