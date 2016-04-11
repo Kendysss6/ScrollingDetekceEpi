fff9bc94760e7b614f587c905ba1788ae49fa45a
Kendysss6 <Kendysss6@users.noreply.github.com>
2016-04-11 18:38:27 +0200

    Prevedl jsem SenzorValue Parceable, protože to je rychlejší než Seriarizable.

--- app/src/main/java/com/example/havlicek/scrollingdetekceepi/SensorValue.java
+++ app/src/main/java/com/example/havlicek/scrollingdetekceepi/SensorValue.java
@@ -1,12 +1,15 @@
 package com.example.havlicek.scrollingdetekceepi;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.text.DecimalFormat;
 
 /**
  * Created by Ondra on 1. 5. 2015.
  * SensorValue, reprezentuje hodnotu naměřenou ze senzoru.
  */
-public class SensorValue {
+public class SensorValue implements Parcelable {
     private long timeStamp;
     private float fX;
     private float fY;
@@ -21,9 +24,11 @@ public class SensorValue {
         this.fX = fX;
         this.fY = fY;
         this.fZ = fZ;
+        /*
         x = decimalFormat.format(fX);
         y = decimalFormat.format(fY);
         z = decimalFormat.format(fZ);
+        */
     }
 
     public SensorValue(long timeStamp, float fX, float fY, float fZ) {
@@ -38,6 +43,7 @@ public class SensorValue {
     public long getTimeStamp() {
         return timeStamp;
     }
+    /*
     public String getX(){
         return x;
     }
@@ -47,6 +53,7 @@ public class SensorValue {
     public String getZ(){
         return z;
     }
+    */
 
     public float getfX() {
         return fX;
@@ -62,5 +69,29 @@ public class SensorValue {
     public String toString() {
         return "Hodnota: "+x+" "+ y+" "+z+" "+Long.toString(timeStamp);
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(timeStamp);
+        dest.writeFloat(fX);
+        dest.writeFloat(fY);
+        dest.writeFloat(fZ);
+    }
+
+    public static final Parcelable.Creator<SensorValue> CREATOR
+            = new Parcelable.Creator<SensorValue>() {
+        public SensorValue createFromParcel(Parcel in) {
+            return new SensorValue(in.readLong(),in.readFloat(), in.readFloat(), in.readFloat(),null);
+        }
+
+        public SensorValue[] newArray(int size) {
+            return new SensorValue[size];
+        }
+    };
 }
 
